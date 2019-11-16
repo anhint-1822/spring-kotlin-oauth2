3f9948d005ce08e3dd7ed575888583baaf5c96d2
tbtrungit <tbtrungit@gmail.com>
2019-11-16 11:06:06 +0700

    Add new config OAuth2 in Project

---
+++ src/main/kotlin/com/baotrung/springkotlincrud/security/ResourceServerConfig.kt
@@ -0,0 +1,33 @@
+package com.baotrung.springkotlincrud.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
+
+
+@Configuration
+@EnableResourceServer
+class ResourceServerConfig : ResourceServerConfigurerAdapter() {
+
+    @Value("\${security.jwt.resource-ids}")
+    private val resourceIds: String? = null
+
+    private val RESOURCE_ID = "resource_id"
+
+    override fun configure(resources: ResourceServerSecurityConfigurer) {
+        resources.resourceId(RESOURCE_ID).stateless(false)
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**", "/api-docs/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+    }
+}
