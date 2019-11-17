34313577241fd0d7fa9850ac8bbda06f4b252bfb
tbtrungit <tbtrungit@gmail.com>
2019-11-18 00:04:11 +0700

    Add new Oauth2

--- src/main/kotlin/com/baotrung/springkotlincrud/security/AuthorizationServerConfig.kt
+++ src/main/kotlin/com/baotrung/springkotlincrud/security/AuthorizationServerConfig.kt
@@ -1,12 +1,14 @@
 package com.baotrung.springkotlincrud.security
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
@@ -17,10 +19,12 @@ import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService
 import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices
+import org.springframework.security.oauth2.provider.token.TokenEnhancer
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain
 import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
+import java.util.*
 import javax.sql.DataSource
 
 @Configuration
@@ -54,52 +58,29 @@ class AuthorizationServerConfig : AuthorizationServerConfigurerAdapter() {
     @Autowired
     private val authenticationManager: AuthenticationManager? = null
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    fun oauthDataSource(): DataSource {
-        return DataSourceBuilder.create().build()
-    }
-
-    @Bean
-    fun clientDetailsService(): JdbcClientDetailsService {
-        return JdbcClientDetailsService(oauthDataSource())
-    }
-
-    @Bean
-    fun tokenStore(): TokenStore {
-        return JdbcTokenStore(oauthDataSource())
-    }
-
-    @Bean
-    fun approvalStore(): ApprovalStore {
-        return JdbcApprovalStore(oauthDataSource())
-    }
-
-    @Bean
-    fun authorizationCodeServices(): AuthorizationCodeServices {
-        return JdbcAuthorizationCodeServices(oauthDataSource())
-    }
+    @Autowired
+    private val passwordEncoder: PasswordEncoder? = null
 
     @Throws(Exception::class)
-    override fun configure(clients: ClientDetailsServiceConfigurer) {
-        clients.withClientDetails(clientDetailsService())
-    }
-
-    @Throws(Exception::class)
-    override fun configure(oauthServer: AuthorizationServerSecurityConfigurer?) {
-
+    override fun configure(configurer: ClientDetailsServiceConfigurer) {
+        configurer
+                .inMemory()
+                .withClient(clientId)
+                .secret(passwordEncoder!!.encode(clientSecret))
+                .authorizedGrantTypes(grantType!!)
+                .scopes(scopeRead, scopeWrite)
+                .resourceIds(resourceIds!!)
     }
 
     @Throws(Exception::class)
     override fun configure(endpoints: AuthorizationServerEndpointsConfigurer) {
         val enhancerChain = TokenEnhancerChain()
-        enhancerChain.setTokenEnhancers(listOf(accessTokenConverter))
+        enhancerChain.setTokenEnhancers(Arrays.asList<TokenEnhancer>(accessTokenConverter!!))
         endpoints.tokenStore(tokenStore)
                 .accessTokenConverter(accessTokenConverter)
                 .tokenEnhancer(enhancerChain)
                 .authenticationManager(authenticationManager)
     }
-
 
 
 }
